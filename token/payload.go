99164a8e8d8e89bb64c962937203f88a6be5ac53
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2026-08-17 00:36:29 +0330

    Add paseto and jwt maker and unit tests

--- token/payload.go
+++ token/payload.go
@@ -1,10 +1,15 @@
 package token
 
 import (
+	"errors"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
+
+var errExpiredToken = errors.New("token has expired")
+var errInvalidToken = errors.New("token is invalid")
 
 type Payload struct {
 	ID        uuid.UUID `json:"id"`
@@ -27,4 +32,35 @@ func NewPayload(username string, duration time.Duration) (*Payload, error) {
 	}
 
 	return payload, nil
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return errExpiredToken
+	}
+	return nil
+}
+
+func (payload *Payload) GetExpirationTime() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(payload.ExpiredAt), nil
+}
+
+func (payload *Payload) GetIssuedAt() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(payload.IssuedAt), nil
+}
+
+func (payload *Payload) GetNotBefore() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(payload.IssuedAt), nil
+}
+
+func (payload *Payload) GetIssuer() (string, error) {
+	return "simpleBank", nil
+}
+
+func (payload *Payload) GetSubject() (string, error) {
+	return payload.Username, nil
+}
+
+func (payload *Payload) GetAudience() (jwt.ClaimStrings, error) {
+	return jwt.ClaimStrings{"simpleBank"}, nil
 }
