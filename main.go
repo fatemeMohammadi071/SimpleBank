066c62fc5f0bf3b084aa302188cc6df305391c2b
fatemeMohammadi071 <96571569+fatemeMohammadi071@users.noreply.github.com>
2026-09-01 12:25:19 +0330

    Add gateway, can call both grpc and http apis

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/simpleBank/gapi"
 	"github.com/simpleBank/pb"
 	"github.com/simpleBank/util"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	_ "github.com/lib/pq"
 	"github.com/simpleBank/api"
@@ -30,6 +34,9 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+
+	// usin this line defin two routes for grpc and http that dose not block each other
+	go runGatewayServer(config, store)
 	runGRPCServer(config, store)
 }
 
@@ -52,6 +59,46 @@ func runGRPCServer(config util.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("Can not start grpc server", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("Can not create server", err)
+	}
+
+	jsonOptions := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOptions)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot create register")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddres)
+	if err != nil {
+		log.Fatal("Can not create listener", err)
+	}
+
+	log.Printf("start http gateway server at %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("Can not start http gateway server", err)
 	}
 }
 
