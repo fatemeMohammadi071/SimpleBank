a5f6f95053f2e5d09e9030d90417a3ae7fae20b7
fatemeMohammadi071 <96571569+fatemeMohammadi071@users.noreply.github.com>
2026-09-01 13:08:05 +0330

    Add clientIp and userAgent for grpc gateway

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -36,12 +36,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
 	}
 
+	mtdt := server.extractMetadat(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIp,
 		IsBlocked:    false,
 		ExpiredAt:    refreshPayload.ExpiredAt,
 	})
